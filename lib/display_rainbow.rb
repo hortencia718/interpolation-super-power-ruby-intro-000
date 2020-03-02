23649800ce190532c36276815c9ca7576ded3a03
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-03-02 17:59:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,13 +1,5 @@
-# Write your #display_rainbow method here
 def display_rainbow(colors)
   # colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
   puts " The first letter in the alphabet is: #{colors[4]},#{colors[3]}, #{colors[6]}, #{colors[1]}, #{colors[0]}, #{colors[2]}, #{colors[5]}"
  end
- # puts "The first color in the alphabet is: #{colors[4]}"
- # puts "The second color in the alphabet is: #{colors[3]}"
- # puts "The third color in the alphabet is: #{colors[5]}"
- # puts "The fourth color in the alphabet is: #{colors[2]}"
- # puts "The fifth color in the alphabet is: #{colors[0]}"
- # puts "The sixth color in the alphabet is: #{colors[1]}"
- # puts "The seventh color in the alphabet is: #{colors[6]}"

fb7cde5d5cb398e5a3b684d2cc8aa32bd0067bd4
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-03-02 17:57:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  # colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
   puts " The first letter in the alphabet is: #{colors[4]},#{colors[3]}, #{colors[6]}, #{colors[1]}, #{colors[0]}, #{colors[2]}, #{colors[5]}"
  end
